660fdcff8bacd1cd77bb66fa39becbb17fe34faa
William Santos <williamdsw@outlook.com>
2021-03-11 18:50:34 -0300

    Refactoring layout and adding more functions

--- Assets/Scripts/Controllers/BandController.cs
+++ Assets/Scripts/Controllers/BandController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,9 +12,17 @@ public class BandController : MonoBehaviour
     [SerializeField] private RectTransform mainPanel;
     [SerializeField] private List<RectTransform> panelsContent;
     [SerializeField] private List<TextMeshProUGUI> panelsHeaders;
+    [SerializeField] private List<Button> clearButtons;
+    [SerializeField] private List<Button> shuffleButtons;
     [SerializeField] private ItemPrefab itemPrefab;
     [SerializeField] private Button quitButton;
+    [SerializeField] private Button resetButton;
+    [SerializeField] private Button rightButton;
+    [SerializeField] private Button leftButton;
 
+    // || Cached References
+
+    private System.Random random = new System.Random();
 
     // || Properties
 
@@ -30,28 +39,17 @@ public class BandController : MonoBehaviour
     {
         Instance = this;
 
-        Bands = new List<Band>()
-        {
-            new Band(1, "Queen", 1970),
-            new Band(2, "Led Zeppelin", 1968),
-            new Band(3, "Black Sabbath", 1968),
-            new Band(4, "The Who", 1964),
-            new Band(5, "Metallica", 1981),
-            new Band(6, "Megadeth", 1983),
-            new Band(7, "Kansas", 1973),
-            new Band(8, "Journey", 1973),
-            new Band(9, "Styx", 1972),
-            new Band(10, "Foo Fighters", 1994),
-        };
-
-        Favorites = new List<Band>();
+        ResetBands(false);
 
         BandItemPrefabs = new List<ItemPrefab>();
         FavoriteItemPrefabs = new List<ItemPrefab>();
 
-        if (quitButton)
+        if (quitButton && resetButton && rightButton && leftButton)
         {
             quitButton.onClick.AddListener(() => Application.Quit());
+            resetButton.onClick.AddListener(() => ResetBands(true));
+            rightButton.onClick.AddListener(() => TransferBands(Bands, Favorites));
+            leftButton.onClick.AddListener(() => TransferBands(Favorites, Bands));
 
             if (Application.platform == RuntimePlatform.WebGLPlayer)
             {
@@ -68,7 +66,9 @@ public class BandController : MonoBehaviour
     public void ListItems()
     {
         if (panelsContent != null && panelsContent.Count == 2 &&
-            panelsHeaders != null && panelsHeaders.Count == 2)
+            panelsHeaders != null && panelsHeaders.Count == 2 &&
+            clearButtons != null && clearButtons.Count == 2 &&
+            shuffleButtons != null && shuffleButtons.Count == 2)
         {
             PropertiesHolder[] holders =
             {
@@ -84,10 +84,26 @@ public class BandController : MonoBehaviour
                 },
             };
 
-            foreach(PropertiesHolder holder in holders)
+            int index = 0;
+            foreach (PropertiesHolder holder in holders)
             {
                 ListItems(holder);
+
+                clearButtons[index].onClick.RemoveAllListeners();
+                clearButtons[index].onClick.AddListener(() =>
+                {
+                    holder.Bands.Clear();
+                    ListItems();
+                });
+                shuffleButtons[index].onClick.RemoveAllListeners();
+                shuffleButtons[index].onClick.AddListener(() => ShuffleList(holder.Bands));
+
+                clearButtons[index].interactable = shuffleButtons[index].interactable = (holder.Bands.Count > 0);
+
+                index++;
             }
+
+            UpdateButtons();
         }
     }
 
@@ -111,11 +127,11 @@ public class BandController : MonoBehaviour
                 holder.ItemPrefabs.Add(item);
             }
 
-            UpdateTitleAndButtons(holder);
+            UpdateTitle(holder);
         }
     }
 
-    private void UpdateTitleAndButtons(PropertiesHolder holder)
+    private void UpdateTitle(PropertiesHolder holder)
     {
         string currentTitle = holder.PanelTitle.text;
         int indexOf = currentTitle.IndexOf("-");
@@ -123,5 +139,62 @@ public class BandController : MonoBehaviour
         string counterText = (holder.ItemPrefabs.Count != 0 ? string.Format("{0} Item(s)", holder.ItemPrefabs.Count) : "Empty");
         currentTitle = string.Format("{0} - {1}", currentTitle, counterText);
         holder.PanelTitle.text = currentTitle;
+    }
+
+    private void UpdateButtons()
+    {
+        rightButton.interactable = (Bands.Count > 0);
+        leftButton.interactable = (Favorites.Count > 0);
+    }
+
+    private void ResetBands(bool render)
+    {
+        Bands = new List<Band>()
+        {
+            new Band(1, "Led Zeppelin", 1968),
+            new Band(2, "The Who", 1964),
+            new Band(3, "Kansas", 1973),
+            new Band(4, "Journey", 1973),
+            new Band(5, "Styx", 1972),
+            new Band(6, "Foo Fighters", 1994),
+        };
+
+        Favorites = new List<Band>()
+        {
+            new Band(7, "Queen", 1970),
+            new Band(8, "Black Sabbath", 1968),
+            new Band(9, "Metallica", 1981),
+            new Band(10, "Megadeth", 1983),
+        };
+
+        if (render)
+        {
+            ListItems();
+        }
+    }
+
+    private void TransferBands(List<Band> source, List<Band> target)
+    {
+        target.AddRange(source);
+        source.Clear();
+        ListItems();
+    }
+
+    private void ShuffleList(List<Band> bands)
+    {
+        List<Band> temp = new List<Band>();
+        temp.AddRange(bands);
+        bands.Clear();
+        while (bands.Count != temp.Count)
+        {
+            int randomIndex = random.Next(0, temp.Count);
+            Band band = temp[randomIndex];
+            if (bands.FindIndex(b => b.Name.ToLower().Equals(band.Name.ToLower())) == -1)
+            {
+                bands.Add(band);
+            }
+        }
+
+        ListItems();
     }
 }
